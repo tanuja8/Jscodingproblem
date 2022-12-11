b929644fb70568aa4580dca708050465273da2a1
Tanuja <tanujakhatun292@gmail.com>
2022-12-11 23:57:44 +0530

    odd number print

--- numberprint.js
+++ numberprint.js
@@ -1,3 +1,12 @@
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// for(let i=1;i<=10;i+=2){
+//     console.log(i);
+// }
+
 for(let i=1;i<=10;i++){
+    if(i%2 !=0)
     console.log(i);
 }
