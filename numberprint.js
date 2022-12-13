fdca910a9a9fbf2fe6282ef84de87b89e7798e0a
Tanuja <tanujakhatun292@gmail.com>
2022-12-14 01:17:12 +0530

    table print problem

--- numberprint.js
+++ numberprint.js
@@ -2,11 +2,11 @@
 //     console.log(i);
 // }
 
-// for(let i=1;i<=10;i+=2){
-//     console.log(i);
-// }
-
-for(let i=1;i<=10;i++){
-    if(i%2 !=0)
+for(let i=1;i<=10;i=i+2){
     console.log(i);
 }
+
+// for(let i=1;i<=10;i++){
+//     if(i%2 !=0)
+//     console.log(i);
+// }
