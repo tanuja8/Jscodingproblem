12c828b8f32e2782a1b5d8bd8c20afcc2647a654
Tanuja <tanujakhatun292@gmail.com>
2022-12-23 23:57:24 +0530

    array problem

---
+++ leet123.js
@@ -0,0 +1,14 @@
+function findTargetIndices(nums, target) {
+    // Sort nums in non-decreasing order
+    nums.sort((a, b) => a - b);
+  
+    // Find the indices of all occurrences of target
+    const indices = [];
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] == target) {
+        indices.push(i);
+      }
+    }
+  
+    return indices;
+  }
