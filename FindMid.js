10425a9a1158e25f783665f570a68cc3ea55bd6d
Tanuja <tanujakhatun292@gmail.com>
2023-03-26 23:43:13 +0530

    array problem solved

---
+++ FindMid.js
@@ -0,0 +1,5 @@
+let arr=[2,8,6,9,4,5,16]
+arr.sort((a,b)=>{
+    return a-b;
+})
+console.log(arr)
---
+++ RemoveDuplicate.js
@@ -0,0 +1,7 @@
+let arr=[1,1,1,2,2,9,15,28,70,70]
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==arr[i]+1){
+        
+    }
+}
+ 
