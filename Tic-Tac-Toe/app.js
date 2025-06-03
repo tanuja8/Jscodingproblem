b418e572d71f7d04b6108cbb2fe3a7d378dd1024
Tanuja <tanujakhatun292@gmail.com>
2025-06-03 20:54:33 +0530

    Update JS file with event handling logic

---
+++ Tic-Tac-Toe/app.js
@@ -0,0 +1,29 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let turn0 = true;
+
+const winningPatterns  = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8]
+];
+
+boxes.forEach((box) => {
+   box.addEventListener("click", () => {
+  console.log("box was clicked");
+  if(turn0){
+    box.innerText="0";
+    turn0=false;
+  }else{
+    box.innerText="X";
+    turn0=true;
+  }
+});
+
+});
