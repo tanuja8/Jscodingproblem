c49f13c06cccaf833cbb50627112bee5fd13037a
Tanuja <tanujakhatun292@gmail.com>
2025-06-05 23:58:20 +0530

    updated

--- Tic-Tac-Toe/app.js
+++ Tic-Tac-Toe/app.js
@@ -3,7 +3,7 @@ let resetBtn = document.querySelector("#reset-btn");
 
 let turn0 = true;
 
-const winningPatterns  = [
+const winningPatterns = [
   [0, 1, 2],
   [0, 3, 6],
   [0, 4, 8],
@@ -11,19 +11,28 @@ const winningPatterns  = [
   [2, 5, 8],
   [2, 4, 6],
   [3, 4, 5],
-  [6, 7, 8]
+  [6, 7, 8],
 ];
 
 boxes.forEach((box) => {
-   box.addEventListener("click", () => {
-  console.log("box was clicked");
-  if(turn0){
-    box.innerText="0";
-    turn0=false;
-  }else{
-    box.innerText="X";
-    turn0=true;
-  }
-});
+  box.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (turn0) {
+      box.innerText = "0";
+      turn0 = false;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
 
+    checkWinner();
+  });
+
+  let checkWinner = () => {
+    for (let pattern of winningPatterns) {
+      console.log(pattern[0],pattern[1],pattern[2]);
+      console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
+    }
+  };
 });
